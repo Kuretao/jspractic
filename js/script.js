2c588b14552be6d3d266bffae63e449333a923c2
Kuretao <Frolov.bogdann@yandex.com>
2025-03-17 12:52:07 +0300

    Первые три задачи - выполнено

--- js/script.js
+++ js/script.js
@@ -24,3 +24,22 @@ const movieDB = {
     ]
 };
 
+
+// первая задача ---------- выполнено
+
+const adsContainer = document.querySelector('.promo__adv');
+const ads = document.querySelectorAll('img');
+ads.forEach(ad => {
+    ad.remove()
+})
+
+
+// вторая задача ---------- выполнено
+
+const promoGenre = document.querySelector('.promo__genre');
+promoGenre.textContent = 'Драма';
+
+// третья задача ---------- выполнено
+
+const promoBg = document.querySelector('.promo__bg');
+promoBg.style.backgroundImage = `url(../img/bg.jpg)`;
